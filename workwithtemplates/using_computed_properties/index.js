cf863c7005d14278b69d0c417504a53c86d7ee3d
Sean Mayer Laptop <m014609f@student.staffs.ac.uk>
2020-05-15 13:34:47 +0100

    Working with Templates - Template interpolations - Binding attributes with v-bind - Using computed properties - Using methods

---
+++ workwithtemplates/using_computed_properties/index.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        slugText: 'The Quick #&$^ Brown 29'
+    },
+    computed:{
+        slugetize: function(){
+            return this.slugText
+            .toLowerCase()
+            .replace(/[^\w ]+/g, '')
+            .replace(/ +/g, '-')
+        }
+    }
+});
---
+++ workwithtemplates/using_methods/index.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        slugText: 'The Quick #&$^ Brown 29'
+    },
+    computed:{
+        slugetize: function(){
+            return this.slugText
+            .toLowerCase()
+            .replace(/[^\w ]+/g, '')
+            .replace(/ +/g, '-') + '-' + this.now()
+            }
+        },
+        methods: {
+            now: function() {
+                var date = new Date();
+                return (
+                        String(date.getHours()) + 
+                        String(date.getMinutes()) + 
+                        String(date.getSeconds())
+                       )
+            }
+        }
+});
