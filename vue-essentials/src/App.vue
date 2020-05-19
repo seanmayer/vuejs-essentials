aac9b21cbb7b396fb9d2c759497ff649d23ae155
Sean Mayer Laptop <m014609f@student.staffs.ac.uk>
2020-05-19 15:54:59 +0100

    Building with the CLI - Installing projects using the Vue CLI - Understanding Vue CLI installations - How CLI components load - Installing additional modules - Testing your module installation

---
+++ vue-essentials/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+
+   <div id="app" class="container mt-5">
+     <h1>My Shop</h1>
+     <p class="animated fadeInRight"> Take a look at our offerings.</p>
+     <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+
+   </div>
+
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+export default {
+  name: 'App',
+  components: {
+    FontAwesomeIcon
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-essentials/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import "bootstrap";
+import {library} from '@fortawesome/fontawesome-svg-core';
+
+import "bootstrap/dist/css/bootstrap.css"
+import "animate.css/animate.css";
+
+import {
+  faShoppingCart,
+  faDollarSign
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faShoppingCart, faDollarSign);
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
